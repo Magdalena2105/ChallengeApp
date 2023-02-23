05c564afa67c6119334bb6b3962406cfcc232543
Magdalena Antosik <antosikmagdalena@wp.pl>
2023-02-23 16:15:40 +0100

    Zadanie domowe dzień 4

---
+++ Zadanie domowe dzień 4/Zadanie domowe dzień 4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Zadanie domowe dzień 4
+var age = 33;
+bool woman = true;
+string name = "Ewa";
+
+if (age < 18 && woman == false)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if (age < 30 && woman == true)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (age == 33 && name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
