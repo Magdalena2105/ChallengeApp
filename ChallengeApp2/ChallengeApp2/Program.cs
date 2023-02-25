b90bf7cc16a540ca401ffda048d0b78a52a1c3b1
Magdalena Antosik <antosikmagdalena@wp.pl>
2023-02-25 18:24:27 +0100

    ChallengeApp2

---
+++ ChallengeApp2/ChallengeApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿int number1 = 5;
+int number2 = 10;
+
+// ==
+// !=
+// <
+// >
+// <=
+// >=
+
+
+var name = "Magdalena";
+var age = 33;
+
+
+bool isActive = true;
+if (!isActive)
+{
+    if (name == "Magdalena")
+    {
+        Console.WriteLine("JESTEM MAGDALENA LUB MAM MNIEJ NIZ 50 LAT.");
+    }
+    else
+    {
+    }
+}
+else if (age > 40)
+{
+    Console.WriteLine("");
+}
+
+
+
+
--- ChellengeApp/ChellengeApp/Program.cs
+++ ChellengeApp/ChellengeApp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// liczby calkowite
+using System;
+
+int myAge = 120;
+int newAge = myAge + 5;
+int myVar = int.MinValue;
+uint myVar2 = uint.MaxValue;
+ulong myVar3 = ulong.MaxValue;
+
+// liczby zmiennoprzecinkowe
+float myNumber = float.MaxValue;
+double myNumber2 = double.MaxValue;
+
+// zmienne tekstowe
+string name = "Magdalena";
+string surname = "Antosik";
+string result = name + surname + myAge;
+//Console.WriteLine(result);
+char myVar5 = 'c';
+var result2 = name.ToArray();
+
+// zmienna logiczna
+bool isActive = true;
+isActive = false;
+var isValid = 5 > 6;
+Console.WriteLine(isValid);
