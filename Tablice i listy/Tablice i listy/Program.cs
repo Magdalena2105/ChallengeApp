b7933f02268d0477d22b7ee7da1b9888f503fdef
Magdalena Antosik <antosikmagdalena@wp.pl>
2023-03-05 19:02:10 +0100

    Zadanie z dnia 5

---
+++ Tablice i listy/Tablice i listy/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] grades = new int[365];
+
+List<string> dayOfWeeks = new List<string>();
+dayOfWeeks.Add("poniedziałek");
+dayOfWeeks.Add("wtorek");
+dayOfWeeks.Add("środa");
+dayOfWeeks.Add("czwartek");
+dayOfWeeks.Add("piątek");
+dayOfWeeks.Add("sobota");
+dayOfWeeks.Add("niedziela");
+
+foreach (var day in dayOfWeeks)
+{
+    Console.WriteLine(day);
+}
